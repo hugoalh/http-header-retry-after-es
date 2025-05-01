482bb473232f4eb7cd2c75e6407199c969f40a8c
hugoalh <hugoalh@gmail.com>
2025-05-01 15:32:12 +0800

    Update resources

--- mod.test.ts
+++ mod.test.ts
@@ -1,6 +1,8 @@
-import { assert } from "STD/assert/assert";
-import { assertEquals } from "STD/assert/equals";
-import { assertThrows } from "STD/assert/throws";
+import {
+	deepStrictEqual,
+	ok,
+	throws
+} from "node:assert";
 import { HTTPHeaderRetryAfter } from "./mod.ts";
 Deno.test("Headers Stringify Number", { permissions: "none" }, () => {
 	const headers = new Headers({
@@ -9,23 +11,23 @@ Deno.test("Headers Stringify Number", { permissions: "none" }, () => {
 	const instance = new HTTPHeaderRetryAfter(headers);
 	const remainTimeMilliseconds = instance.getRemainTimeMilliseconds();
 	const remainTimeSeconds = instance.getRemainTimeSeconds();
-	assert(remainTimeMilliseconds > 0 && remainTimeMilliseconds <= 120000);
-	assert(remainTimeSeconds > 0 && remainTimeSeconds <= 120);
+	ok(remainTimeMilliseconds > 0 && remainTimeMilliseconds <= 120000);
+	ok(remainTimeSeconds > 0 && remainTimeSeconds <= 120);
 });
 Deno.test("Headers Timestamp GMT", { permissions: "none" }, () => {
 	const headers = new Headers({
 		"Retry-After": "Wed, 21 Oct 2015 07:28:00 GMT"
 	});
 	const instance = new HTTPHeaderRetryAfter(headers);
-	assertEquals(instance.getDate().valueOf(), 1445412480000);
-	assertEquals(instance.getRemainTimeMilliseconds(), 0);
-	assertEquals(instance.getRemainTimeSeconds(), 0);
+	deepStrictEqual(instance.getDate().valueOf(), 1445412480000);
+	deepStrictEqual(instance.getRemainTimeMilliseconds(), 0);
+	deepStrictEqual(instance.getRemainTimeSeconds(), 0);
 });
 Deno.test("Headers Timestamp ISO8601", { permissions: "none" }, () => {
 	const headers = new Headers({
 		"Retry-After": "2011-10-05T14:48:00.000Z"
 	});
-	assertThrows(() => {
+	throws(() => {
 		new HTTPHeaderRetryAfter(headers);
 	});
 });
@@ -33,17 +35,17 @@ Deno.test("Raw Stringify Number", { permissions: "none" }, () => {
 	const instance = new HTTPHeaderRetryAfter("120");
 	const remainTimeMilliseconds = instance.getRemainTimeMilliseconds();
 	const remainTimeSeconds = instance.getRemainTimeSeconds();
-	assert(remainTimeMilliseconds > 0 && remainTimeMilliseconds <= 120000);
-	assert(remainTimeSeconds > 0 && remainTimeSeconds <= 120);
+	ok(remainTimeMilliseconds > 0 && remainTimeMilliseconds <= 120000);
+	ok(remainTimeSeconds > 0 && remainTimeSeconds <= 120);
 });
 Deno.test("Raw Timestamp GMT", { permissions: "none" }, () => {
 	const instance = new HTTPHeaderRetryAfter("Wed, 21 Oct 2015 07:28:00 GMT");
-	assertEquals(instance.getDate().valueOf(), 1445412480000);
-	assertEquals(instance.getRemainTimeMilliseconds(), 0);
-	assertEquals(instance.getRemainTimeSeconds(), 0);
+	deepStrictEqual(instance.getDate().valueOf(), 1445412480000);
+	deepStrictEqual(instance.getRemainTimeMilliseconds(), 0);
+	deepStrictEqual(instance.getRemainTimeSeconds(), 0);
 });
 Deno.test("Raw Timestamp ISO8601", { permissions: "none" }, () => {
-	assertThrows(() => {
+	throws(() => {
 		new HTTPHeaderRetryAfter("2011-10-05T14:48:00.000Z");
 	});
 });
